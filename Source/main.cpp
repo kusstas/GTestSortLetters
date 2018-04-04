102ec3b0077001a5248af01621f0e9897898284b
Stanislav Kusovsky <kusovsky95@gmail.com>
2018-04-04 12:03:12 +0300

    add empty string

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,6 +12,7 @@ const char* cyrillicWithLetters = "QWERTYUIOPAASDFGHJKLіваЕНРИZXCVBNMqwer
 
 TEST (testValidString, onlyLetters)
 {
+    ASSERT_EQ(true, isValidString(""));
     ASSERT_EQ(true, isValidString(lowLetters));
     ASSERT_EQ(true, isValidString(upLetters));
     ASSERT_EQ(true, isValidString(allLetters));
@@ -26,16 +27,19 @@ TEST (testValidString, notOnlyLetters)
 
 TEST (testSort, onlyLetters)
 {
+    char str0[] = "";
     char str1[] = "zxakb";
     char str2[] = "XZAKB";
     char str3[] = "ZXKzxkABab";
     char str4[] = "BBABSDDSSzczxzxc";
 
+    ASSERT_EQ(true, sortLetters(str0));
     ASSERT_EQ(true, sortLetters(str1));
     ASSERT_EQ(true, sortLetters(str2));
     ASSERT_EQ(true, sortLetters(str3));
     ASSERT_EQ(true, sortLetters(str4));
 
+    ASSERT_STREQ("", str0);
     ASSERT_STREQ("abkxz", str1);
     ASSERT_STREQ("ABKXZ", str2);
     ASSERT_STREQ("ABKXZabkxz", str3);
